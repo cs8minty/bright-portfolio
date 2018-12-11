c124f8d4d1b7a380445c4bc2f98fca655ae9db72
unknown <cs8minty@gmail.com>
2018-12-11 15:58:18 +0900

    Jumbotron 컴포넌트 생성

---
+++ src/components/Jumbotron.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const Box = styled.div`
+  border: 1px solid black;
+`;
+
+const Jumbotron = () => {
+  return <Box>Hello World</Box>;
+};
+
+export default Jumbotron;
